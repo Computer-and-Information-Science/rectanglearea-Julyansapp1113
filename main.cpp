8c0f7c52e97e82e9b5347eef8b9ab2b16ad3da37
Julyan Sapp <Julyan11d13sapp@gmail.com>
2025-09-09 23:44:34 +0000

    program can calculate area and perimeter of a rectangle

--- main.cpp
+++ main.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 
 int main() {
-  const double length = 7.2;
-  const double width = 1.3;
+  // Define dimensions
+  double length = 7.2;
+  double width = 1.3;
 
+  // Calculate area and perimeter
+  double area = length * width;
+  double perimeter = 2 * (length + width);
 
+  // Display results
+  cout << "Rectangle properties:" << endl;
+  cout << "Length = " << length << endl;
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << static_cast<int>(perimeter) << endl;
+
+  return 0;
 }
